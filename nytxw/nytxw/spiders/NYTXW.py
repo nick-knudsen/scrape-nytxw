554abc7a7f79077919fdf64bb20dc2a1fedebe98
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-20 12:47:07 -0500

    working on authentication

--- nytxw/nytxw/spiders/NYTXW.py
+++ nytxw/nytxw/spiders/NYTXW.py
@@ -1,4 +1,13 @@
 import scrapy
+import toml
+
+def get_secrets():
+    secret_path = 'secrets.toml'
+    secrets = toml.load(secret_path)
+    username = secrets['NYTXW']['username']
+    password = secrets['NYTXW']['password']
+
+    return username, password
 
 def authentication_failed(response):
     pass
@@ -9,4 +18,10 @@ class NytxwSpider(scrapy.Spider):
     start_urls = ["https://www.nytimes.com/crosswords/game/daily/1993/11/21"]
 
     def parse(self, response):
+        username, password = get_secrets()
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'username': username, 'password': password},
+            callback=self.after_login
+        )
         pass
