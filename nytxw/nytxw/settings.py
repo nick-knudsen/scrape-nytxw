a8bbbb01d36e32faf5c15b3d1cbc15a68580be04
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-24 19:34:10 -0500

    Disobey robots.txt

--- nytxw/nytxw/settings.py
+++ nytxw/nytxw/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "nytxw.spiders"
 #USER_AGENT = "nytxw (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
